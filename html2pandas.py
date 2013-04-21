964519828be6b620c3981285f6b373099f40454c
Yaser Martinez Palenzuela <yaser.martinez@gmail.com>
2013-04-21 02:03:26 +0200

    added dtype

--- html2pandas.py
+++ html2pandas.py
@@ -2,10 +2,10 @@ from pyquery import PyQuery as pq
 from pandas import DataFrame
 import numpy as np
 
-def convert(html):
+def convert(html, dtype=object):
     d = pq(html)
     columns = d('thead tr').eq(0).text().split()
     #index_columns = d('thead tr').eq(1).text().split()
     n_rows = len(d('tbody tr'))
-    values = np.array(d('tbody tr td').text().split()).reshape(n_rows, len(columns))
+    values = np.array(d('tbody tr td').text().split(), dtype=dtype).reshape(n_rows, len(columns))
     return DataFrame(values, columns=columns)
